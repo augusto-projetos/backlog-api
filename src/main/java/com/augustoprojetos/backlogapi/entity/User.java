3f8af448e543dde06d8f261136fd8e30b4d78ba1
Luiz Augusto Leite Freitas <luiz.alfreitas12@gmail.com>
2026-01-28 11:44:39 -0300

    feat: adiciona sistema de segurança

---
+++ src/main/java/com/augustoprojetos/backlogapi/entity/User.java
@@ -0,0 +1,78 @@
+package com.augustoprojetos.backlogapi.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor // Lombok: Cria construtor vazio
+@AllArgsConstructor // Lombok: Cria construtor com tudo
+// implements UserDetails
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String login;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    // --- Métodos obrigatórios do UserDetails ---
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Por enquanto, todo mundo é USER comum.
+        // No futuro, teremos ADMIN aqui.
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login; // Avisa pro Spring que nosso "username" é o campo "login"
+    }
+
+    @Override
+    public String getPassword() {
+        return password; // Avisa onde está a senha
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // A conta nunca expira
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // A conta nunca é bloqueada
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // A senha nunca vence
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // O usuário está sempre ativo
+    }
+}
