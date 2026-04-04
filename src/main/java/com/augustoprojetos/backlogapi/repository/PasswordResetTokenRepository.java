5ebe19c085f80ac871e4b060861df8d85e652694
Luiz Freitas <luiz.alfreitas12@gmail.com>
2026-04-04 15:23:04 -0300

    fix: conserta os bugs das melhorias (#37)
    - fix: resolve problema de token duplicado
    - feat: adiciona sistema de segurança

--- src/main/java/com/augustoprojetos/backlogapi/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/augustoprojetos/backlogapi/repository/PasswordResetTokenRepository.java
@@ -1,9 +1,11 @@
 package com.augustoprojetos.backlogapi.repository;
 
 import com.augustoprojetos.backlogapi.entity.PasswordResetToken;
+import com.augustoprojetos.backlogapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+    Optional<PasswordResetToken> findByUser(User user);
 }
