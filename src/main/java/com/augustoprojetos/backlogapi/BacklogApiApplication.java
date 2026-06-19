1803a2fb3fa0f6bc46cc229dff7f9a7f19a489f8
Luiz Augusto Leite Freitas <luiz.alfreitas12@gmail.com>
2026-06-18 21:11:33 -0300

    feat: sistema de limpeza automática no banco de dados

--- src/main/java/com/augustoprojetos/backlogapi/BacklogApiApplication.java
+++ src/main/java/com/augustoprojetos/backlogapi/BacklogApiApplication.java
@@ -4,11 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class BacklogApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/augustoprojetos/backlogapi/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/augustoprojetos/backlogapi/repository/EmailVerificationTokenRepository.java
@@ -3,6 +3,8 @@ package com.augustoprojetos.backlogapi.repository;
 import com.augustoprojetos.backlogapi.entity.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
@@ -12,4 +14,7 @@ public interface EmailVerificationTokenRepository extends JpaRepository<EmailVer
     
     // Útil caso a gente precise deletar tokens antigos de um usuário específico
     Optional<EmailVerificationToken> findByUser_Id(Long userId);
+
+    // Busca todos os tokens onde a data de expiração é menor (antes) que a data atual
+    List<EmailVerificationToken> findByExpiryDateBefore(LocalDateTime now);
 }
