4ac2da54b2864b568bef881be650a94e68ae0f2f
Luiz Freitas <luiz.alfreitas12@gmail.com>
2026-07-02 17:24:47 -0300

    fix: altera fuso horário para America/Sao_Paulo

--- src/main/java/com/augustoprojetos/backlogapi/BacklogApiApplication.java
+++ src/main/java/com/augustoprojetos/backlogapi/BacklogApiApplication.java
@@ -1,17 +1,25 @@
 package com.augustoprojetos.backlogapi;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class BacklogApiApplication {
+
+    @PostConstruct
+    public void init() {
+        // Força a JVM usar o fuso horário de Brasília
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     public static void main(String[] args) {
         // 1. Carrega o arquivo .env
